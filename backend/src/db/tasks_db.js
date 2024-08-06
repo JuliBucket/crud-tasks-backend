cf0be063a94c53a8bdc47176429e82bdbdd17b5a
julibucket <juliansaucedo92@gmail.com>
2024-08-06 00:00:28 -0300

    se actualizo la estructura de archivos

---
+++ backend/src/db/tasks_db.js
@@ -0,0 +1,14 @@
+const mysql2 = require('mysql2/promise')
+
+const newConnection = async () => {
+  const connection = await mysql2.createConnection ({
+    host: "localhost",
+    user: "root",
+    database: "tasks_db"
+  })
+  return connection
+}
+
+module.exports = {
+  newConnection
+}
